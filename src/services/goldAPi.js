ee938670346c7239d7100be5aff76146334d62d2
danielsmpe <danielsampe61@gmail.com>
2026-02-03 10:40:42 +0700

    Add Gold API integration service

---
+++ src/services/goldAPi.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+const fetchGoldPrice = async () => {
+  const apikey = process.env.GOLD_API_KEY;
+  const url = `https://www.goldapi.io/api/XAU/USD`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "x-access-token": apikey,
+        "Content-Type": "application/json",
+      },
+    });
+    return {
+      price: response.data.price,
+      priceGram: response.data.price_gram_24k,
+      change: response.data.ch,
+      changePercent: response.data.chp,
+      timestamp: response.data.timestamp,
+    };
+  } catch (error) {
+    console.error("Error fetching gold price:", error);
+    return null;
+  }
+};
+module.exports = { fetchGoldPrice };
